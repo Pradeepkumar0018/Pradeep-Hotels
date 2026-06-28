b597775f241867c07ccf39c4e058cfe6997dc751
K PRADEEP KUMAR <kpradeepkumar3005@gmail.com>
2026-06-28 18:00:17 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,12 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.NODE_ENV === 'production' ? '/Food-nextjs' : '';
-
 const nextConfig = {
   output: 'export',
-  basePath,
-  assetPrefix: basePath,
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
   },
 };
 
